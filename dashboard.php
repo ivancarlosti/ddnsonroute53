d715f0651f35032d19ee09f1483f631b4b6c002e
Ivan Carlos <ivan@ivancarlos.com.br>
2025-03-23 21:43:33 -0300

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -4,6 +4,12 @@ if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true){
     header('location: index.php');
     exit;
 }
+
+// Get the current domain of the hosted page
+$domain = $_SERVER['HTTP_HOST']; // This will automatically get the domain of the hosted page
+
+// Construct the cURL command
+$curlCommand = "https://[USERNAME]:[PASSWORD]@$domain/update.php?hostname=[DOMAIN]&myip=[IP]";
 ?>
 <!DOCTYPE html>
 <html>
@@ -16,5 +22,10 @@ if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true){
     <p><a href="manage_aws.php">Manage AWS Credentials</a></p>
     <p><a href="manage_ddns.php">Manage DDNS Entries</a></p>
     <p><a href="index.php">Logout</a></p>
+
+    <!-- Display the cURL command -->
+    <h2>DDNS Update cURL Command</h2>
+    <p>Use the following cURL command to update your DDNS entry:</p>
+    <pre><?php echo htmlspecialchars($curlCommand); ?></pre>
 </body>
 </html>
