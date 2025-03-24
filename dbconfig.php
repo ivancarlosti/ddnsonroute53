b7090eeb9140457c92f0b2154c490368e1d30889
Ivan Carlos <ivan@ivancarlos.com.br>
2025-03-24 19:52:21 -0300

    Update dbconfig.php

--- dbconfig.php
+++ dbconfig.php
@@ -1,9 +1,10 @@
 <?php
 // dbconfig.php
-define('DB_SERVER', getenv('DB_SERVER') ?: 'YOURDBHOST');
-define('DB_USERNAME', getenv('DB_USERNAME') ?: 'YOURDBUSERNAME');
-define('DB_PASSWORD', getenv('DB_PASSWORD') ?: 'YOURDBPASSWORD');
-define('DB_NAME', getenv('DB_NAME') ?: 'YOURDBNAME');
+define('DB_SERVER', getenv('DB_SERVER') ?: 'your_database_host');
+define('DB_NAME', getenv('DB_NAME') ?: 'your_database_name');
+define('DB_USERNAME', getenv('DB_USERNAME') ?: 'your_database_user');
+define('DB_PASSWORD', getenv('DB_PASSWORD') ?: 'your_database_password');
+
 
 // Create the connection (EXACTLY as in your original)
 $link = @new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
